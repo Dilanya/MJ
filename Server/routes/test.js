0c89a672a5042b927e43a72d24fecdfbcc68a484
Dilanya <96423@fhss.sjp.ac.lk>
2023-08-23 21:29:16 +0530

    test route added

--- Server/routes/test.js
+++ Server/routes/test.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// Define a test route
+
 router.get('/test', (req, res) => {
   res.status(200).send('Server is up and running.');
 });
