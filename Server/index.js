cbda868a9ab4a980bf148fb3a997201f9c37cdcf
Dilanya <96423@fhss.sjp.ac.lk>
2023-08-26 20:32:21 +0530

    up

--- Server/index.js
+++ Server/index.js
@@ -12,7 +12,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '10mb'}));
  //Serve uploaded files statically
 app.use('/uploads', express.static('uploads'));
 
