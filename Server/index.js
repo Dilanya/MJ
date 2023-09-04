a6a17ca912dc6ee9a4309e47cd6a34defbd53a67
Dilanya <96423@fhss.sjp.ac.lk>
2023-09-04 12:23:15 +0530

    up

--- Server/index.js
+++ Server/index.js
@@ -5,21 +5,10 @@ const bodyParser = require('body-parser');
 const port = 3000;
 
 
-const allowedOrigins = [
-  'https://pawlific-shop.myshopify.com',
-  'https://demo-mid-journey-api.myshopify.com'
-];
-
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: '*', 
   optionsSuccessStatus: 200,
-  credentials: true
+  credentials: true, 
 };
 
 app.use(cors(corsOptions));
